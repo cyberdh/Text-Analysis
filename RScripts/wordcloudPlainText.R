d9a32138eb463bfed6738c0166731ccb53452bcf
ctgniady <ctgniady@iu.edu>
2016-01-28 15:59:14 -0500

    creating Notebook

--- RScripts/wordcloudPlainText.R
+++ RScripts/wordcloudPlainText.R
@@ -1,12 +1,13 @@
 #Wordcloud of Top 75 Words in Jane Eyre
 
 #Set the working directory
-setwd("~/Desktop/R/RStyleometry/plainText/bronte")
+setwd("~/Desktop/R/Text_Analysis/data/bronte")
 
 
 #Call libraries used in the script
-library(wordcloud)
-library(tm)
+#library(wordcloud)
+#library(tm)
+#library(Rcolorbrewer)
 
 #Read in the text 
 text_raw<-scan("janeEyre.txt", what="character", sep="\n")
@@ -20,5 +21,5 @@ corpus <- tm_map(corpus, content_transformer(tolower))
 corpus <- tm_map(corpus, removeWords, stopwords("SMART"))
 corpus <- tm_map(corpus, removePunctuation)
 
-wordcloud(corpus,random.order=FALSE,scale=c(3,.1),rot.per=0,min.freq=1,
+wordcloud(corpus,random.order=FALSE,scale=c(3,.1),rot.per=0,
           max.words=75,colors=brewer.pal(8, "Dark2"))
