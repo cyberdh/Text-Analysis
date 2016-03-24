ce497c8352e5f7025d84ca76d88b23e3bb3e91c0
gracethom <gracethomas@149-161-137-80.dhcp-bl.indiana.edu>
2016-03-24 13:42:20 -0400

    updated files

---
+++ ShinyNew/Twitter/app.R
@@ -0,0 +1,41 @@
+library(shiny)
+library(wordcloud)
+library(qdap)
+library(RColorBrewer)
+
+setwd("~/Documents/IU/CyberDH/Text_Analysis/ShinyNew/Twitter")
+source("helpers.R")
+
+ui <- fluidPage(
+  titlePanel("Twitter Conversation on the Brussels Attack"),
+  
+  sidebarLayout(
+    sidebarPanel(
+      helpText("This interactive plot shows the most frequently tweeted words in reference to the Brussels attack on March 22, 2016"),
+      
+
+      sliderInput("range", 
+                  label = "Number of Words:",
+                  min = 1, max = 100, value = 50)
+      
+    ),
+    
+    mainPanel(
+      plotOutput("plot")
+    )
+  )
+  
+)
+
+server <- function(input, output) {
+  wordcloud_rep <- repeatable(wordcloud)
+  
+  output$plot <- renderPlot({
+    wordcloud_rep(words,freqs,scale=c(5,1.25),min.freq=3,max.words=input$range, rot.per=0, 
+                  colors=cols)
+  })
+  
+  
+}
+
+shinyApp(ui = ui, server = server)
