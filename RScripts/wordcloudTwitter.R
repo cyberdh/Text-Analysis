a8b925654ddb6070d17776e8e106722a790739e3
ctgniady <ctgniady@iu.edu>
2016-06-23 11:08:17 -0400

    using Pulse data

--- RScripts/wordcloudTwitter.R
+++ RScripts/wordcloudTwitter.R
@@ -4,7 +4,7 @@ library(RColorBrewer)
 
 setwd("~/Desktop/R/Text_Analysis/data/twitter/")
 
-load("pulse2016-06-16.RData")
+load("orlando2016-06-16.RData")
 tweetlist <- sapply(tweets, function(x) x$text)
 
 #Strip URLS
@@ -17,7 +17,7 @@ tweetlist=gsub( "[^[:alnum:] ]", "", tweetlist )
 words <-strsplit(tweetlist, "\\W+", perl=TRUE)
 
 # #Remove common words
-words=rm_stopwords(words,c(Top100Words,"rt", "amp", "pulse"))
+words=rm_stopwords(words,c(Top100Words,"rt", "amp", "orlando", "https"))
 
 #Get rid of empty elements
 words=words[lapply(words,length)>0]
