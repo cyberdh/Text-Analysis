b430d6ca4b2c22f60cef2764a983fe75ee29be1b
ctgniady <ctgniady@iu.edu>
2016-06-23 21:55:19 -0400

    updated to pulse

--- shiny/TwitterWordcloud/app.R
+++ shiny/TwitterWordcloud/app.R
@@ -1,24 +1,20 @@
 library(shiny)
+library(rsconnect)
 library(wordcloud)
 library(qdap)
 library(RColorBrewer)
 
+#setwd("~/Desktop/R/Text_Analysis/Shiny/Twitter")
 
 source("helpers.R")
 
 
-
-# setwd("~/Documents/IU/CyberDH/Text_Analysis/Shiny/Twitter")
-
-#source("helpers.R")
-
-
 ui <- fluidPage(
-  titlePanel("Twitter Conversation After the Death of Prince"),
+  titlePanel("Twitter Conversation After the Shooting at the Pulse Nightclub"),
   
   sidebarLayout(
     sidebarPanel(
-      helpText("This interactive plot shows the most frequently tweeted words in reference to the death of the musician Price on April 22, 2016"),
+      helpText("This interactive plot shows the most frequently tweeted words in reference to the shooting at the Pulse Nightclub"),
       sliderInput("freq", 
                   label = "Minimum frequency of words:",
                   min = 1, max = 400, value = 3),
@@ -38,16 +34,14 @@ ui <- fluidPage(
 
 server <- function(input, output) {
   
-  #words <- read.csv("words.csv", sep= ",")
-  #freqs <- read.csv("freqs.csv", sep= ",")
-  
   cols <- colorRampPalette(brewer.pal(12,"Paired"))(500)
 
   wordcloud_rep <- repeatable(wordcloud)
   
   output$plot <- renderPlot({
-    wordcloud_rep(words,freqs, scale=c(4,1),min.freq=input$freq,max.words=input$range, rot.per=0, 
-                  colors=cols)
+
+    wordcloud_rep(words,freqs,scale=c(5,1),max.words=75, rot.per=0, 
+                     colors=brewer.pal(8, "Dark2"))
   })
   
   
