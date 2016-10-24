e76188a1ec10f4729c69f659b94887bccb7b3df8
Anna VanderJagt <avanderj@indiana.edu>
2016-10-24 19:59:37 -0400

    changed path, added comments

--- RScripts/wordcloudTwitter.R
+++ RScripts/wordcloudTwitter.R
@@ -1,7 +1,9 @@
+#Call libraries used in the script 
 library(wordcloud)
 library(qdap)
 library(RColorBrewer)
 
+#Set the working directory
 setwd("~/Desktop/Text-Analysis/data/twitter/")
 
 load("~/Desktop/Text-Analysis/data/twitter/hillary2016-07-28.RData")
