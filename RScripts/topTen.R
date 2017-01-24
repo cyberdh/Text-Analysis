e05a93b3669a813e6cc863a70b530b13f90de734
David K <klosteda@iu.edu>
2017-01-24 14:49:19 -0500

    Update topTen.R

--- RScripts/topTen.R
+++ RScripts/topTen.R
@@ -15,8 +15,8 @@ corpus <- Corpus(VectorSource(text_raw))
 corpus <- tm_map(corpus, content_transformer(tolower))
 #To change the stopword list, use other dictionaries available with the tm package
 #Add early modern stopwords by u adding "myStopWords
-#myStopWords <- scan("data/earlyModernStopword.txt", what="character", sep="\n")
-corpus <- tm_map(corpus, removeWords, stopwords("SMART"))
+myStopWords <- scan("data/earlyModernStopword.txt", what="character", sep="\n")
+corpus <- tm_map(corpus, removeWords, c(stopwords("english"), myStopWords))
 corpus <- tm_map(corpus, removePunctuation)
 corpus <- tm_map(corpus, stripWhitespace)
 corpus <- tm_map(corpus, PlainTextDocument)
