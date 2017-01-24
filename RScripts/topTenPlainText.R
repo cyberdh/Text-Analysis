135482844bd9b0cfd1ac9171c144f01809749f1d
David K <klosteda@iu.edu>
2017-01-24 14:45:52 -0500

    Update topTenPlainText.R

--- RScripts/topTenPlainText.R
+++ RScripts/topTenPlainText.R
@@ -17,7 +17,7 @@ corpus <- tm_map(corpus, content_transformer(tolower))
 #To change the stopword list, use other dictionaries available with the tm package
 #Add early modern stopwords by u adding "myStopWords
 myStopWords <- scan("data/earlyModernStopword.txt", what="character", sep="\n")
-corpus <- tm_map(corpus, removeWords, c(stopwords("SMART"), myStopWords))
+corpus <- tm_map(corpus, removeWords, c(stopwords("english"), myStopWords))
 corpus <- tm_map(corpus, removePunctuation)
 corpus <- tm_map(corpus, stripWhitespace)
 corpus <- tm_map(corpus, PlainTextDocument)
@@ -28,7 +28,7 @@ freq <- sort(colSums(as.matrix(dtm)), decreasing = TRUE)
 #Remix ideas: 1) Play around with the number of word on the chart by changing it from "10"
 # 2) Don't forget the adjust your plot's title if you have changed the input text
 # 3) Look the frequency of all the words in the corpus by typing "freq" into the console
-plot(head(freq, 10), type="b", lwd=2, col="blue", col.lab="red", main="Lear, Entire Play", xlab="Top Ten Words", ylab="Number of Occurences", xaxt="n",)
+plot(head(freq, 10), type="b", lwd=2, col="blue", col.lab="red", main="Hamlet, Entire Play", xlab="Top Ten Words", ylab="Number of Occurences", xaxt="n",)
 axis(1,1:10, cex=.5, labels=names(head(freq, 10)))
 
 
