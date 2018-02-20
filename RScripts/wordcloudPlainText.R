71728356ae8e7100c0777c3dfaca2784524a314c
David K <klosteda@iu.edu>
2018-02-20 13:58:02 -0500

    Update wordcloudPlainText.R

--- RScripts/wordcloudPlainText.R
+++ RScripts/wordcloudPlainText.R
@@ -24,7 +24,7 @@ myStopWords <- scan("data/earlyModernStopword.txt", what="character", sep="\n")
 corpus <- tm_map(corpus, removeWords, c(stopwords("english"), myStopWords))
 corpus <- tm_map(corpus, removePunctuation)
 corpus <- tm_map(corpus, stripWhitespace)
-corpus <- tm_map(corpus, PlainTextDocument)
+#corpus <- tm_map(corpus, PlainTextDocument)
 
 wordcloud(corpus,random.order=FALSE,scale=c(4,1),rot.per=0,
           max.words=75,colors=brewer.pal(8, "Dark2"))
