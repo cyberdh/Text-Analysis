467da1e85c1110163af40f28d7a7a130e0fd73cb
David K <klosteda@iu.edu>
2018-02-23 11:36:33 -0500

    Update topTenTwitter.R

--- RScripts/topTenTwitter.R
+++ RScripts/topTenTwitter.R
@@ -13,7 +13,7 @@ outDir<-"data/twitter"
 unzip(zipF,exdir=outDir)
 
 #Read in the text 
-tweets <- read.csv("data/twitter/neverAgain.csv")
+tweets <- read.csv("data/twitter/neverAgainSnippet.csv")
 
 #Create a corpus 
 corpus <- iconv(tweets$text, to = "utf-8")
