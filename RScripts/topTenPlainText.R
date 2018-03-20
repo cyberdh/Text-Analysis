f74c9dacdf57b3369785ad16b71ca5a5a040e26f
David K <klosteda@iu.edu>
2018-03-20 11:58:53 -0400

    Update topTenPlainText.R

--- RScripts/topTenPlainText.R
+++ RScripts/topTenPlainText.R
@@ -1,4 +1,4 @@
-#Top Ten Words in Hamlet, Curated Stopwords Removed
+#Top Ten Words in an Episode of Star Trek: The Next Generation, Curated Stopwords Removed
 #Call libraries used in the script
 #Set the working directory
 setwd("~/Text-Analysis/")
@@ -8,7 +8,7 @@ library(tm)
 
 
 #Read in the text 
-text_raw<-scan("data/shakespeareFolger/Hamlet.txt", what="character", sep="\n")
+text_raw<-scan("data/StarTrekNextGenClean/series/277.txt", what="character", sep="\n")
 
 #Create a corpus 
 corpus <- Corpus(VectorSource(text_raw))
@@ -29,8 +29,9 @@ freq <- sort(colSums(as.matrix(dtm)), decreasing = TRUE)
 #Remix ideas: 1) Play around with the number of word on the chart by changing it from "10"
 # 2) Don't forget the adjust your plot's title if you have changed the input text
 # 3) Look the frequency of all the words in the corpus by typing "freq" into the console
-barplot(head(freq, 10), ylim=c(0,250), col=c("red3", "orange3","yellow3","green3","blue3","darkorchid3","darkred", "darkorange", "gold", "darkgreen"), col.lab="red",
-     main="Hamlet, Entire Play", xlab="Top Ten Words", ylab="Number of Occurences", xaxt="s")
+par(mar=c(15,4,4,0))
+barplot(head(freq, 10), ylim=c(0,100), col=c("red3", "orange3","yellow3","green3","blue3","darkorchid3","darkred", "darkorange", "gold", "darkgreen"), col.main="Gold", col.lab="red", col.axis="gray28", las=2,
+     main="Star Trek: The Next Generation, Final Episode", xlab="Top Ten Words", ylab="Number of Occurences", xaxt="s")
 
 
 
