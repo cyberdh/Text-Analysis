2523d499e5ea26bad9e07b8ad6619946d950993c
David K <klosteda@iu.edu>
2018-07-23 18:30:55 -0400

    Add files via upload

--- RScripts/topTenTwitter.R
+++ RScripts/topTenTwitter.R
@@ -19,37 +19,45 @@ tweets <- read.csv("data/twitter/neverAgain.csv")
 corpus <- iconv(tweets$text, to = "utf-8")
 corpus <- Corpus(VectorSource(corpus))
 
-#Clean the corpus
-corpus <- tm_map(corpus, content_transformer(function(x) iconv(x, "ASCII", "UTF-8", sub="")))
-corpus <- tm_map(corpus, content_transformer(tolower))
-#To change the stopword list, use other dictionaries available with the tm package
-#Add early modern stopwords by u adding "myStopWords
-myStopWords <- scan("data/earlyModernStopword.txt", what="character", sep="\n")
-corpus <- tm_map(corpus, removePunctuation)
-corpus <- tm_map(corpus, stripWhitespace)
-removeURL <- function(x) gsub("http[[:alnum:]]*", "", x)
-corpus <- tm_map(corpus, content_transformer(removeURL))
-corpus <- tm_map(corpus, removeWords, c(stopwords("english"), myStopWords, 'amp','rt','neveragain','dont', 'emma4change', 'xokarminox'))
+corpus <- tm_map(corpus, function(x) iconv(x, "ASCII", "UTF-8", sub=""))
 
-dtm <- DocumentTermMatrix(corpus)
-freq <- sort(colSums(as.matrix(dtm)), decreasing = TRUE)
+#Clean the corpus
+corpus <- tolower(corpus)
+corpus <- removePunctuation(corpus)
+corpus <- removeNumbers(corpus)
+corpus <- stripWhitespace(corpus)
+removeURL <- function(x) gsub("http[[:alnum:]]*", "", x)
+corpus <- removeURL(corpus)
+corpus <- removeWords(corpus, c(stopwords("english"), 'na','amp','rt', 'neveragain', 'dont', 'will', 'see','just', 'emmachange', 'xokarminox'))
+
+myCorpus <- as.character(corpus)
 
 #Convert to data frame (helps make plot labeling easier)
-freqDF <- as.data.frame.character(freq)
-freqDF$words <- rownames(freqDF)
+freqDF <- data_frame(myCorpus) %>%
+  unnest_tokens(word, myCorpus) %>%
+  count(word, sort = TRUE)
 
-labNames <- head(freqDF$words, 10)
+head(freqDF)
+
+#freqDF <- freqDF[-1,]
+
+#head(freqDF)
+
+labNames <- head(freqDF$word, 10)
+
+write.csv(head(freqDF, 100), "~/freqWordsNeverAgain.csv")
 
 #Remix ideas: 1) Play around with the number of word on the chart by changing it from "10"
-# 2) Don't forget the adjust your plot's title if you have changed the input text
-# 3) Look the frequency of all the words in the corpus by typing "freq" into the console
+# 2) Don't forget to adjust your plot's title if you have changed the input text
+# 3) Look up the frequency of all the words in the corpus by typing "freq" into the console
 
 par(mar=c(8,5,3,1))
-x <- barplot(head(freq,10), ylim=c(0,8000), col=c("red3", "orange3","yellow3","green3","blue3","darkorchid3","darkred", "darkorange", "gold", "darkgreen"), col.lab="black",
-     main="Tweet Word Frequency", xlab="", ylab="Number of Occurences", xaxt="n")
+x <- barplot(head(freqDF$n,10), ylim=c(0,8000), col=c("red3", "orange3","yellow3","green3","blue3","darkorchid3","darkred", "darkorange", "gold", "darkgreen"),col.main="gold", col.lab="red",
+             main="Tweet Word Frequency", xlab="", ylab="Number of Occurences", xaxt="n")
 axis(2, at=x, labels=TRUE)
-text(x, par("usr")[3], srt=45, adj=c(1.1,1.5), xpd = TRUE, labels = labNames, cex=.9)
-title(xlab='Top Ten Words', line=5)
+axis(2, col="gray27", col.axis="gray27", col.ticks="gray27",labels=TRUE)
+text(x, par("usr")[3], srt=45, adj=c(1.1,1.5), xpd = TRUE, labels = labNames, cex=.9, col="gray27")
+title(xlab='Top Ten Words', line=5, col.lab="red")
 print(x)
 
 
