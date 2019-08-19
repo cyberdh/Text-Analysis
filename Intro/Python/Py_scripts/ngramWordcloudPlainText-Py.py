d9ae90e68f45bdd39a2f3a4b97d3bfb88d5c26d0
David K <klosteda@iu.edu>
2019-08-19 17:07:14 -0400

    Add files via upload

--- Intro/Python/Py_scripts/ngramWordcloudPlainText-Py.py
+++ Intro/Python/Py_scripts/ngramWordcloudPlainText-Py.py
@@ -1,9 +1,17 @@
+# NOTE: This chunk of code is only for use with Research Desktop. You will get an error if you try to run this code on your personal device!!
+
+import sys
+import os
+sys.path.insert(0,"/N/u/cyberdh/Carbonate/dhPyEnviron/lib/python3.6/site-packages")
+os.environ["NLTK_DATA"] = "/N/u/cyberdh/Carbonate/dhPyEnviron/nltk_data"
+
+
+# Include necessary packages for notebook 
+
 from textblob import TextBlob
 from nltk.corpus import stopwords
 import nltk
-import os
 import re
-
 import string
 import pandas as pd
 from collections import Counter, defaultdict
@@ -13,21 +21,61 @@ from PIL import Image
 import numpy as np
 import operator
 import glob
-
 import matplotlib.pyplot as plt
 
+get_ipython().magic('matplotlib inline')
 
 
-stop_words = stopwords.words('english')
-stop_words.extend(['would', 'said', 'says', 'good'])
+# File paths
 
-def textClean(text, stopWordsList = None):
+homePath = os.environ["HOME"]
+dataHome = os.path.join(homePath, "Text-Analysis-master", "data", "shakespeareDated")
+dataResults = os.path.join(homePath, "Text-Analysis-master", "Output")
+
+
+# Set needed variables
+
+data = "*.txt"
+nltkStop = True
+customStop = True
+ng = 2
+stopLang = 'english'
+stopWords = []
+cleanText = []
+ngramList = []
+
+#print(" ".join(stopwords.fileids()))
+
+
+# Stopwords
+
+# NLTK Stop words
+if nltkStop is True:
+    stopWords.extend(stopwords.words(stopLang))
+
+    stopWords.extend(['would', 'said', 'says', 'also'])
+
+
+# Add own stopword list
+
+if customStop is True:
+    stopWordsFilepath = os.path.join(homePath, "Text-Analysis-DavidBranchV2", "data", "earlyModernStopword.txt")
+
+    with open(stopWordsFilepath, "r",encoding = 'utf-8') as stopfile:
+        stopWordsCustom = [x.strip() for x in stopfile.readlines()]
+
+    stopWords.extend(stopWordsCustom)
+
+
+# Functions
+
+# Text Cleaning
+
+def textClean(text):
     
     text = text.strip().lower()
     
-    tweets = re.sub(r"http\S+", "", text)
-    
-    tokens = re.split(r'\W+', tweets )
+    tokens = re.split(r'\W+', text )
     
     # remove empty string
     tokens = [t for t in tokens if t]
@@ -36,11 +84,7 @@ def textClean(text, stopWordsList = None):
     tokens = [t for t in tokens if not t.isdigit()]
     
     # built-in stop words list
-    tokens = [t for t in tokens if t not in stop_words]
-    
-    # custom stop words list
-    if stopWordsList is not None:
-        tokens = [t for t in tokens if t not in stopWordsList]
+    tokens = [t for t in tokens if t not in stopWords]
         
     # remove punctuation
     puncts = list(string.punctuation)
@@ -50,76 +94,88 @@ def textClean(text, stopWordsList = None):
 
     return tokens
 
-# load custom stop words list
 
-#stopWordsFilepath = '/Users/klosteda/Text-Analysis/data/earlyModernStopword.txt'
+# Reading in the Text
 
-dataHome = '/N/u/klosteda/Carbonate/Text-Analysis/data/StarTrekNextGenClean/season1'
-
-#with open(stopWordsFilepath, "r") as f:
-    #stopWordsList = f.readlines()
-            
-#stopWordsList = [w.strip().lower() for w in stopWordsList]
-
-cleanText = []
-
-for path in glob.glob(os.path.join(dataHome, "102.txt")):
-    with open(path, "r") as f:
+for path in glob.glob(os.path.join(dataHome, data)):
+    with open(path, "r") as file:
          # skip hidden file
         if path.startswith('.'):
             continue
-        text = f.read()
+        text = file.read()
         cleanText.extend(textClean(text))
-        
 
-    print('Finished tokenizing text {}\n'.format(path))
-    
+
+# Convert text to a str object so we can find ngrams later.
 
 cleanTokens = ' '.join(cleanText)
 
+
+# Find Ngrams
+
 blob = TextBlob(cleanTokens)
 
-unigrams = blob.ngrams(n=1)
-bigrams = blob.ngrams(n=2)
-trigrams = blob.ngrams(n=3)
+if ng == 1: 
+    nGrams = blob.ngrams(n=1)
+if ng == 2:
+    nGrams = blob.ngrams(n=2)
+if ng == 3:
+    nGrams = blob.ngrams(n=3)
 
-ngramList = []
-for wlist in bigrams:
+
+# Convert ngrams to a list
+
+for wlist in nGrams:
    ngramList.append(' '.join(wlist))
 
+
+# Now we make our dataframe. 
 df = pd.DataFrame(ngramList)
 df = df.replace(' ', '_', regex=True)
 dfCounts = df[0].value_counts()
 countsDF = pd.DataFrame(dfCounts)
-df_C = countsDF.rename(columns={'index':'ngrams', 0:'freq'})
-df_C = df_C.sort_values('freq', ascending = False)
+countsDF.reset_index(inplace = True)
+df_C = countsDF.rename(columns={'index':'ngrams',0:'freq'})
+df_C.set_index(df_C['ngrams'], inplace = True)
+df_C['ngrams'] = df_C['ngrams'].astype(str)
+dfNG = df_C.sort_values('freq', ascending = False)
 
-df_C.head(10)
 
-maskPath = '/N/u/klosteda/Carbonate/Text-Analysis/data/wordcloudMasks'
-mask = np.array(Image.open(os.path.join(maskPath, "Enterprise.png")))
+# Now lets see what our dataframe looks like. 
+dfNG.head(10)
 
-text = df_C
 
-stopwords = set(STOPWORDS)
-stopwords.update(["columns", "rows"])
-    
-wc = wordcloud.WordCloud(background_color = "white", colormap = 'Dark2', mask = mask, stopwords = stopwords).generate_from_frequencies(text['freq'])
+# Plot our wordcloud
+
+# Variables
+useMask = True
+maskPath = os.path.join(homePath, 'Text-Analysis-master','data','wordcloudMasks')
+mask = np.array(Image.open(os.path.join(maskPath, "Shakespeare.png")))
+maxWrdCnt = 500
+bgColor = "black"
+color = "Dark2"
+figureSz = (80,40)
+wcOutputFile = "ngramWordCloud.png"
+imgFmt = "png"
+dpi = 300
+
+# Ngram Stopwords
+stopwords = ["ngrams","good_lord","come_come"]
+text = dfNG[~dfNG['ngrams'].isin(stopwords)]
+
+# Wordcloud aesthetics
+if useMask is True:    
+    wc = wordcloud.WordCloud(background_color = bgColor, max_words = maxWrdCnt, colormap = color, mask = mask).generate_from_frequencies(text['freq'])
+else:
+    wc = wordcloud.WordCloud(background_color = bgColor, max_words = maxWrdCnt, colormap = color, mask = None).generate_from_frequencies(text['freq'])
 
 # show
-plt.figure(figsize = (20, 10))
+plt.figure(figsize = figureSz)
 plt.imshow(wc, interpolation = 'bilinear')
 plt.axis("off")
-
 plt.tight_layout()
     
-# save graph as a png image to file
-plt.savefig('/N/u/klosteda/Carbonate/Text-Analysis/Output/ngramWordCloudStarTrek.png', format = 'png', dpi = 300, bbox_inches = 'tight')
+# save graph as an image to file
+plt.savefig(os.path.join(dataResults, wcOutputFile), format = imgFmt, dpi = dpi, bbox_inches = 'tight')
     
-    
-
-
 plt.show()
-
-
-
